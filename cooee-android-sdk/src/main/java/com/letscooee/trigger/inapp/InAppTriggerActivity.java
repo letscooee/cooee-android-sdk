1702db28f4001e76c1e525f90ed70a7576ece1ac
Ashish Gaikwad <ashish@wizpanda.com>
2021-07-02 20:32:10 +0530

    refactor: unity and full screen change

--- cooee-android-sdk/src/main/java/com/letscooee/trigger/inapp/InAppTriggerActivity.java
+++ cooee-android-sdk/src/main/java/com/letscooee/trigger/inapp/InAppTriggerActivity.java
@@ -120,6 +120,15 @@ public class InAppTriggerActivity extends AppCompatActivity implements PreventBl
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
         this.isFreshLaunch = savedInstanceState == null;
         setContentView(R.layout.activity_engagement_trigger);
 
