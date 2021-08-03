5205d2ce906115414711ee202e3a2908a945fac8
Ashish Gaikwad <ashish@wizpanda.com>
2021-08-03 18:28:15 +0530

    refactor: Update document in PermissionType COOEE-66

--- cooee-android-sdk/src/main/java/com/letscooee/utils/PermissionType.java
+++ cooee-android-sdk/src/main/java/com/letscooee/utils/PermissionType.java
@@ -7,6 +7,13 @@ import androidx.annotation.NonNull;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Type of permission going to ask in app
+ *
+ * @author Ashish Gaikwad 03/08/21
+ * @since 1.0.0
+ */
+
 public enum PermissionType {
 
     LOCATION(Manifest.permission.ACCESS_FINE_LOCATION),
@@ -18,6 +25,8 @@ public enum PermissionType {
     private static final Map<String, PermissionType> lookup = new HashMap<>();
 
     /**
+     * Constructor to write value of {@link PermissionType}
+     *
      * @param text
      */
     PermissionType(final String text) {
@@ -30,12 +39,23 @@ public enum PermissionType {
         }
     }
 
+    /**
+     * Returns value of {@link PermissionType}
+     *
+     * @return Returns value of {@link PermissionType} present in {@link #text}
+     */
     @NonNull
     @Override
     public String toString() {
         return text;
     }
 
+    /**
+     * Convert {@code value} to {@link PermissionType}
+     *
+     * @param value appropriate value of specific {@link PermissionType}
+     * @return Returns {@link PermissionType} for the given <code>value</code> or null
+     */
     public static PermissionType getByValue(String value) {
         return lookup.get(value);
     }
