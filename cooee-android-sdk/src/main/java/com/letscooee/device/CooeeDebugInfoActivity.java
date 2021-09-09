2cbb1c07eb4550af1f12c0595788f5bdbe436cfb
Ashish Gaikwad <ashish@wizpanda.com>
2021-09-09 12:33:31 +0530

    feat: Add password protection to view debug information while in release mode COOEE-76

--- cooee-android-sdk/src/main/java/com/letscooee/device/CooeeDebugInfoActivity.java
+++ cooee-android-sdk/src/main/java/com/letscooee/device/CooeeDebugInfoActivity.java
@@ -4,8 +4,10 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -13,21 +15,99 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.letscooee.BuildConfig;
 import com.letscooee.R;
 import com.letscooee.trigger.inapp.PreventBlurActivity;
+import com.letscooee.utils.Constants;
 import com.letscooee.utils.DebugInfoCollector;
 
+import java.util.Calendar;
 import java.util.Map;
 import java.util.TreeMap;
 
+/**
+ * Show debug information for inter use only
+ *
+ * @author Ashish Gaikwad 09/09/21
+ * @since 1.0.0
+ */
 public class CooeeDebugInfoActivity extends AppCompatActivity implements PreventBlurActivity {
+
+    private final String ACTIVITY_PASSWORD;
+    private static boolean IS_USER_AUTHORIZED_PREVIOUSLY = false;
+
+    public CooeeDebugInfoActivity() {
+        Calendar calendar = Calendar.getInstance();
+        int month = (calendar.get(Calendar.MONTH) + 1) + Constants.INCREMENT_PASSWORD;
+        int minute = calendar.get(Calendar.MINUTE) + Constants.INCREMENT_PASSWORD;
+        ACTIVITY_PASSWORD = minute + "" + month;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cooee_debug_info);
+
+        // If SDK is in DEBUG mode debug information will shown directly
+        if (BuildConfig.DEBUG) {
+            showInfoScreen();
+            return;
+        }
+
+        // Check if user is authorized in current session
+        if (IS_USER_AUTHORIZED_PREVIOUSLY) {
+            showInfoScreen();
+        } else {
+            showPasswordScreen();
+        }
+    }
+
+    /**
+     * If SDK is in release mode and use is not authorized in current session password screen
+     * will appear.
+     */
+    private void showPasswordScreen() {
+        setContentView(R.layout.view_password);
+
+        Button button = findViewById(R.id.btnNext);
+        button.setOnClickListener(v -> checkPasswordAndProceed());
+
         ImageView imageViewClose = findViewById(R.id.imageViewClose);
         imageViewClose.setOnClickListener(v -> finish());
+    }
+
+    /**
+     * Access entered password and will check for valid password.
+     * If password is valid then debug information screen will be loaded.
+     */
+    private void checkPasswordAndProceed() {
+        TextInputEditText passwordEditText = findViewById(R.id.edtPassword);
+        TextView passwordWarning = findViewById(R.id.tvPasswordWarning);
+
+        String password = passwordEditText.getText().toString();
+
+        if (TextUtils.isEmpty(password)) {
+            passwordWarning.setVisibility(View.VISIBLE);
+            passwordWarning.setText("Enter Password");
+            return;
+        } else if (!password.equals(ACTIVITY_PASSWORD)) {
+            passwordWarning.setVisibility(View.VISIBLE);
+            passwordWarning.setText("Entered Invalid Password");
+            return;
+        }
+
+        passwordWarning.setVisibility(View.INVISIBLE);
+        passwordEditText.setText("");
+        IS_USER_AUTHORIZED_PREVIOUSLY = true;
+        showInfoScreen();
+    }
+
+    /**
+     * Fetch and show all debug information on the screen.
+     */
+    private void showInfoScreen() {
+        setContentView(R.layout.activity_cooee_debug_info);
+
         LinearLayout linearLayoutInfo = findViewById(R.id.linearLayoutInfo);
 
         ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
@@ -51,10 +131,13 @@ public class CooeeDebugInfoActivity extends AppCompatActivity implements Prevent
                 view.setOnClickListener(v -> {
                     ClipData clip = ClipData.newPlainText(property.getKey(), property.getValue().toString());
                     clipboard.setPrimaryClip(clip);
-                    Toast.makeText(this, property.getKey()+" Copied", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, property.getKey() + " Copied", Toast.LENGTH_SHORT).show();
                 });
                 linearLayoutInfo.addView(view);
             }
         }
+
+        ImageView imageViewClose = findViewById(R.id.imageViewClose);
+        imageViewClose.setOnClickListener(v -> finish());
     }
 }
