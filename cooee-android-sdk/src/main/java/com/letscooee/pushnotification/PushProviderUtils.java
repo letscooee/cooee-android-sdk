0c3d49654fd3121dc12f3f1d834f8851e8023f87
Ashish Gaikwad <ashish@letscooee.com>
2022-03-08 16:12:09 +0530

    feat ✨: Introduce getPendingIntent to get PendentIntent for service COOEE-615

--- cooee-android-sdk/src/main/java/com/letscooee/pushnotification/PushProviderUtils.java
+++ cooee-android-sdk/src/main/java/com/letscooee/pushnotification/PushProviderUtils.java
@@ -1,5 +1,9 @@
 package com.letscooee.pushnotification;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 import androidx.annotation.RestrictTo;
 import com.letscooee.CooeeFactory;
@@ -34,5 +38,38 @@ public class PushProviderUtils {
 
     public static String getLastSentToken() {
         return lastSentToken;
+    }
+
+    /**
+     * Creates a PendingIntent for the given context.
+     * Android 12 added more security with the PendingIntents. Now we need to tell that if PendingIntent
+     * can be mutated or not.
+     *
+     * @param context     The context.
+     * @param requestCode The request code.
+     * @param intent      The intent.
+     * @return The PendingIntent.
+     * @see <a href="https://developer.android.com/about/versions/12/behavior-changes-12#pending-intent-mutability">
+     * Android 12 PendingIntent Mutability</a>
+     * @see <a href="https://developer.android.com/reference/android/app/PendingIntent#FLAG_IMMUTABLE"> FLAG_IMMUTABLE</a>
+     * @see <a href="https://developer.android.com/reference/android/app/PendingIntent#FLAG_MUTABLE"> FLAG_MUTABLE</a>
+     * @since 1.3.5
+     */
+    public static PendingIntent getPendingIntentService(Context context, int requestCode, Intent intent) {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP) {
+            return PendingIntent.getService(
+                    context,
+                    requestCode,
+                    intent,
+                    PendingIntent.FLAG_ONE_SHOT
+            );
+        }
+
+        return PendingIntent.getService(
+                context,
+                requestCode,
+                intent,
+                PendingIntent.FLAG_IMMUTABLE
+        );
     }
 }
