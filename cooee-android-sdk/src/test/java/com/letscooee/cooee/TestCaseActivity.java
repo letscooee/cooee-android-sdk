694b832fec106a308131672f894a8cb7ef260b97
Ashish Gaikwad <ashish@letscooee.com>
2022-03-31 11:02:46 +0530

    test 🚨: Add dummy activity for captureWindowForBlurryEffect COOEE-632

---
+++ cooee-android-sdk/src/test/java/com/letscooee/cooee/TestCaseActivity.java
@@ -0,0 +1,15 @@
+package com.letscooee.cooee;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.letscooee.R;
+
+public class TestCaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.in_app_trigger_activity);
+    }
+}
