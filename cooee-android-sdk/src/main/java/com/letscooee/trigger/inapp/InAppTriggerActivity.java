db09ea8f40763e082bad71d983fa72214529b691
Ashish Gaikwad <ashish@letscooee.com>
2022-04-20 17:42:04 +0530

    refactor 📦: Format code COOEE-698

--- cooee-android-sdk/src/main/java/com/letscooee/trigger/inapp/InAppTriggerActivity.java
+++ cooee-android-sdk/src/main/java/com/letscooee/trigger/inapp/InAppTriggerActivity.java
@@ -100,11 +100,11 @@ public class InAppTriggerActivity extends AppCompatActivity implements PreventBl
         } else {
             //noinspection deprecation
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
-            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_IMMERSIVE
-            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
         }
     }
 
