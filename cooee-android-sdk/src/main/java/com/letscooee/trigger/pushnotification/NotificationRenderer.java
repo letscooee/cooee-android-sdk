f3a9a1cb710dfc0a673c199622d85ca9d4d18014
Ashish Gaikwad <ashish@letscooee.com>
2022-06-22 10:46:56 +0530

    refactor 📦: Expect sentAt as date and add it to PNs setWhen COOEE-767

--- cooee-android-sdk/src/main/java/com/letscooee/trigger/pushnotification/NotificationRenderer.java
+++ cooee-android-sdk/src/main/java/com/letscooee/trigger/pushnotification/NotificationRenderer.java
@@ -134,10 +134,11 @@ public abstract class NotificationRenderer {
     }
 
     private void setBuilder() {
+        Date sentAt = triggerData.getSentAt() == null ? new Date() : triggerData.getSentAt();
+
         this.notificationBuilder
                 .setAutoCancel(cancelPushOnClick())
-                // TODO: 11/06/21 It should be the date of engagement trigger planned
-                .setWhen(System.currentTimeMillis())
+                .setWhen(sentAt.getTime())
                 .setSmallIcon(context.getApplicationInfo().icon)
                 .setCustomContentView(smallContentViews)
                 .setCustomBigContentView(bigContentViews)
