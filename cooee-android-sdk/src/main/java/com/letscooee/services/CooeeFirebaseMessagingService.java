e8805a0d783ba323f9956a957d9cdf3c5082d193
Shashank Agrawal <shashank@letscooee.com>
2022-06-23 13:06:14 +0530

    refactor: Remove notification buttons code for now COOEE-797

--- cooee-android-sdk/src/main/java/com/letscooee/services/CooeeFirebaseMessagingService.java
+++ cooee-android-sdk/src/main/java/com/letscooee/services/CooeeFirebaseMessagingService.java
@@ -1,29 +1,22 @@
 package com.letscooee.services;
 
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.util.Log;
 import androidx.annotation.NonNull;
-import androidx.core.app.NotificationCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.google.gson.JsonSyntaxException;
 import com.letscooee.CooeeFactory;
-import com.letscooee.R;
 import com.letscooee.enums.trigger.PendingTriggerAction;
 import com.letscooee.font.FontProcessor;
 import com.letscooee.models.Event;
 import com.letscooee.models.trigger.TriggerData;
-import com.letscooee.models.trigger.elements.ButtonElement;
-import com.letscooee.models.trigger.push.PushNotificationTrigger;
 import com.letscooee.pushnotification.PushProviderUtils;
 import com.letscooee.room.trigger.PendingTrigger;
 import com.letscooee.trigger.EngagementTriggerHelper;
 import com.letscooee.trigger.cache.PendingTriggerService;
 import com.letscooee.trigger.pushnotification.SimpleNotificationRenderer;
 import com.letscooee.utils.Constants;
-import com.letscooee.utils.PendingIntentUtility;
 
 import java.util.Map;
 
@@ -135,39 +128,10 @@ public class CooeeFirebaseMessagingService extends FirebaseMessagingService {
     private void showNotification(TriggerData triggerData) {
         SimpleNotificationRenderer renderer = new SimpleNotificationRenderer(context, triggerData);
         renderer.setContentIntent();
-        renderer.addActions(createActionButtons(triggerData.getPn(), renderer.getNotificationID()));
         renderer.render();
 
         pendingTrigger.notificationId = renderer.getNotificationID();
         pendingTriggerService.loadAndSaveTriggerData(pendingTrigger, triggerData);
     }
 
-    private NotificationCompat.Action[] createActionButtons(PushNotificationTrigger triggerData, int notificationID) {
-        if (triggerData.getButtons() == null) {
-            return new NotificationCompat.Action[0];
-        }
-
-        NotificationCompat.Action[] actions = new NotificationCompat.Action[triggerData.getButtons().size()];
-        int requestCode = notificationID;
-        int i = 0;
-        for (ButtonElement triggerButton : triggerData.getButtons()) {
-            String title = triggerButton.getText();
-
-            Intent actionButtonIntent = new Intent(getApplicationContext(), PushNotificationIntentService.class);
-            actionButtonIntent.setAction(Constants.ACTION_PUSH_BUTTON_CLICK);
-            actionButtonIntent.putExtra(Constants.INTENT_TRIGGER_DATA_KEY, triggerData);
-            actionButtonIntent.putExtra("notificationId", notificationID);
-
-            PendingIntent pendingIntent = PendingIntentUtility.getService(
-                    getApplicationContext(),
-                    requestCode++,
-                    actionButtonIntent
-            );
-
-            actions[i++] = new NotificationCompat.Action(R.drawable.common_google_signin_btn_icon_dark, title, pendingIntent);
-
-        }
-
-        return actions;
-    }
 }
