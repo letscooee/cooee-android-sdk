bdedf221df1bbdc7ef404563747e542d743603dd
Ashish Gaikwad <ashish@letscooee.com>
2022-06-29 13:17:17 +0530

    refactor 📦: Add Serializers in Json Adapters COOEE-797 COOEE-796

--- cooee-android-sdk/src/main/java/com/letscooee/trigger/adapters/PushTypeDeserializer.java
+++ cooee-android-sdk/src/main/java/com/letscooee/trigger/adapters/PushTypeDeserializer.java
@@ -2,10 +2,13 @@ package com.letscooee.trigger.adapters;
 
 import androidx.annotation.RestrictTo;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 import com.letscooee.enums.trigger.PushType;
 
 import java.lang.reflect.Type;
@@ -19,7 +22,10 @@ import java.lang.reflect.Type;
  * @since 1.3.12
  */
 @RestrictTo(RestrictTo.Scope.LIBRARY)
-public class PushTypeDeserializer implements JsonDeserializer<PushType> {
+public class PushTypeDeserializer implements JsonDeserializer<PushType>, JsonSerializer<PushType> {
+
+    private final Gson gson = new Gson();
+
     @Override
     public PushType deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         int pushType = json.getAsInt();
@@ -32,5 +38,17 @@ public class PushTypeDeserializer implements JsonDeserializer<PushType> {
             default:
                 return PushType.SIMPLE;
         }
+    }
+
+    @Override
+    public JsonElement serialize(PushType src, Type typeOfSrc, JsonSerializationContext context) {
+        if (src == null) {
+            return null;
+        }
+
+        if (src == PushType.CAROUSEL) {
+            return gson.toJsonTree(2);
+        }
+        return gson.toJsonTree(1);
     }
 }
