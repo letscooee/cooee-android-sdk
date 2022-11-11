3bffe8b5128a2ebc64f9736a2aef80a0f43c442e
Ashish Gaikwad <ashish@letscooee.com>
2022-11-11 13:40:18 +0530

    refactor: Expose logout method in CooeeSDK COOEE-1068
    Add missing method document and add document URLs in method document

--- cooee-android-sdk/src/main/java/com/letscooee/CooeeSDK.java
+++ cooee-android-sdk/src/main/java/com/letscooee/CooeeSDK.java
@@ -27,16 +27,13 @@ import java.util.Map;
 public class CooeeSDK {
 
     private static final String SYSTEM_DATA_PREFIX = "CE ";
-
-    private static CooeeSDK cooeeSDK;
-
     private final Context context;
-    private final RuntimeData runtimeData;
-    private final SentryHelper sentryHelper;
-    private final DeviceAuthService deviceAuthService;
-    private final SafeHTTPService safeHTTPService;
-
+    private static CooeeSDK cooeeSDK;
     private WeakReference<CooeeCTAListener> ctaListener;
+    private final DeviceAuthService deviceAuthService;
+    private final RuntimeData runtimeData;
+    private final SafeHTTPService safeHTTPService;
+    private final SentryHelper sentryHelper;
 
     /**
      * Private constructor for Singleton Class
@@ -53,11 +50,40 @@ public class CooeeSDK {
         CooeeExecutors.getInstance().singleThreadExecutor().execute(this.deviceAuthService::acquireSDKToken);
     }
 
+    public CooeeCTAListener getCTAListener() {
+        return ctaListener == null ? null : ctaListener.get();
+    }
+
+    /**
+     * Set call back listener to the Cooee SDK.
+     * This call back listener will be fired when the user performs any action on the
+     * InApp/Notification sent via Cooee.
+     *
+     * @param listener instance of the {@link CooeeCTAListener}
+     * @see <a href="https://docs.letscooee.com/developers/android/cta-callback">CTA Callback</a>
+     * for more details.
+     */
+    public void setCTAListener(CooeeCTAListener listener) {
+        ctaListener = new WeakReference<>(listener);
+    }
+
+    /**
+     * Get manually updated screen name
+     *
+     * @return current screen name
+     */
+    @Deprecated
+    public String getCurrentScreenName() {
+        return this.runtimeData.getCurrentScreenName();
+    }
+
     /**
      * Create and return default instance for CooeeSDK (Singleton Class)
      *
      * @param context application context
-     * @return CooeeSDK
+     * @return {@link CooeeSDK} instance.
+     * @see <a href="https://docs.letscooee.com/developers/android/get-started#OSeGV">
+     * Get Started</a> for more details.
      */
     public static CooeeSDK getDefaultInstance(Context context) {
         if (cooeeSDK == null) {
@@ -65,6 +91,30 @@ public class CooeeSDK {
         }
 
         return cooeeSDK;
+    }
+
+    @Deprecated
+    public String getUUID() {
+        return this.getUserID();
+    }
+
+    /**
+     * Provides <code>userId</code> assigned by Cooee to the user.
+     *
+     * @return userID in {@link String} format
+     */
+    public String getUserID() {
+        return this.deviceAuthService.getUserID();
+    }
+
+    /**
+     * Logout user from the Cooee SDK.
+     * This method must be called with apps logout functionality.
+     *
+     * @since 1.4.2
+     */
+    public void logOut() {
+        CooeeExecutors.getInstance().singleThreadExecutor().execute(this.safeHTTPService::logOutUser);
     }
 
     /**
@@ -87,7 +137,7 @@ public class CooeeSDK {
      * @param eventProperties Properties associated with the event
      * @throws PropertyNameException if property name starts with "CE "
      * @throws NullPointerException  if eventName is null
-     * @see <a href="https://docs.letscooee.com/developers/android/track-events">Track Events</a> for
+     * @see <a href="https://docs.letscooee.com/developers/android/tracking-events">Track Events</a> for
      * more details.
      */
     public void sendEvent(String eventName, Map<String, Object> eventProperties)
@@ -104,6 +154,47 @@ public class CooeeSDK {
     }
 
     /**
+     * Set current screen name where user navigated.
+     *
+     * @param screenName Name of the screen. Like Login, Cart, Wishlist, etc.
+     * @see <a href="https://docs.letscooee.com/developers/android/tracking-screen">Tracking Screens</a>
+     * for more details
+     */
+    public void setCurrentScreen(String screenName) {
+        if (TextUtils.isEmpty(screenName)) {
+            Log.v(Constants.TAG, "Trying to set empty screen name");
+            return;
+        }
+
+        // Update screen name on main thread, because other threads may be in paused state till CPU gets free.(Edge case scenario)
+        String previousScreenName = this.runtimeData.getCurrentScreenName();
+        this.runtimeData.setCurrentScreenName(screenName);
+
+        CooeeExecutors.getInstance().singleThreadExecutor().execute(() -> {
+            /*
+             * Properties will hold previous screen name.
+             */
+            Map<String, Object> properties = new HashMap<>();
+            properties.put("ps", previousScreenName);
+
+            Event event = new Event(Constants.EVENT_SCREEN_VIEW, properties);
+
+            this.safeHTTPService.sendEvent(event);
+        });
+
+    }
+
+    /**
+     * Launch {@link DebugInfoActivity} activity which holds debug information.
+     * This information is useful to debug problem with the SDK.
+     */
+    public void showDebugInfo() {
+        Intent intent = new Intent(context, DebugInfoActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        context.startActivity(intent);
+    }
+
+    /**
      * Send given user data to the server
      *
      * @param userData The common user data like name, email.
@@ -113,18 +204,6 @@ public class CooeeSDK {
     @Deprecated
     public void updateUserData(Map<String, Object> userData) throws PropertyNameException {
         updateUserProfile(userData, null);
-    }
-
-    /**
-     * Send given user properties to the server
-     *
-     * @param userProperties The additional user properties.
-     * @throws PropertyNameException if property name starts with "CE "
-     * @deprecated use {@link CooeeSDK#updateUserProfile(Map)} instead
-     */
-    @Deprecated
-    public void updateUserProperties(Map<String, Object> userProperties) throws PropertyNameException {
-        updateUserProfile(null, userProperties);
     }
 
     /**
@@ -155,7 +234,8 @@ public class CooeeSDK {
      * @param userData The common user data like name, email, etc.
      * @throws PropertyNameException if property name starts with "CE "
      * @throws NullPointerException  if userData is null
-     * @see <a href="https://docs.letscooee.com/developers/android/track-props">Tracking User Properties</a>
+     * @see <a href="https://docs.letscooee.com/developers/android/tracking-properties">Tracking User Properties</a>
+     * for more details.
      */
     public void updateUserProfile(Map<String, Object> userData) throws PropertyNameException, NullPointerException {
         if (userData == null) {
@@ -170,69 +250,15 @@ public class CooeeSDK {
     }
 
     /**
-     * Set current screen name where user navigated.
+     * Send given user properties to the server
      *
-     * @param screenName Name of the screen. Like Login, Cart, Wishlist etc.
-     */
-    public void setCurrentScreen(String screenName) {
-        if (TextUtils.isEmpty(screenName)) {
-            Log.v(Constants.TAG, "Trying to set empty screen name");
-            return;
-        }
-
-        // Update screen name on main thread, because other threads may be in paused state till CPU gets free.(Edge case scenario)
-        String previousScreenName = this.runtimeData.getCurrentScreenName();
-        this.runtimeData.setCurrentScreenName(screenName);
-
-        CooeeExecutors.getInstance().singleThreadExecutor().execute(() -> {
-            /*
-             * Properties will hold previous screen name.
-             */
-            Map<String, Object> properties = new HashMap<>();
-            properties.put("ps", previousScreenName);
-
-            Event event = new Event(Constants.EVENT_SCREEN_VIEW, properties);
-
-            this.safeHTTPService.sendEvent(event);
-        });
-
-    }
-
-    /**
-     * Get manually updated screen name
-     *
-     * @return current screen name
+     * @param userProperties The additional user properties.
+     * @throws PropertyNameException if property name starts with "CE "
+     * @deprecated use {@link CooeeSDK#updateUserProfile(Map)} instead
      */
     @Deprecated
-    public String getCurrentScreenName() {
-        return this.runtimeData.getCurrentScreenName();
-    }
-
-    @Deprecated
-    public String getUUID() {
-        return this.getUserID();
-    }
-
-    public String getUserID() {
-        return this.deviceAuthService.getUserID();
-    }
-
-    public void setCTAListener(CooeeCTAListener listener) {
-        ctaListener = new WeakReference<>(listener);
-    }
-
-    public CooeeCTAListener getCTAListener() {
-        return ctaListener == null ? null : ctaListener.get();
-    }
-
-    /**
-     * Launch {@link DebugInfoActivity} activity which holds debug information.
-     * This information is useful to debug problem with the SDK.
-     */
-    public void showDebugInfo() {
-        Intent intent = new Intent(context, DebugInfoActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        context.startActivity(intent);
+    public void updateUserProperties(Map<String, Object> userProperties) throws PropertyNameException {
+        updateUserProfile(null, userProperties);
     }
 
     /**
@@ -252,4 +278,5 @@ public class CooeeSDK {
             }
         }
     }
+
 }
